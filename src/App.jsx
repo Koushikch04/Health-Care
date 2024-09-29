488d82d10c9035407a7bc2e17020ec2769b4b74f
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2024-09-29 16:48:21 +0530

    deployment testing-5

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 
 import useAlert from "./hooks/useAlert";
-import Layout from "./components/Layout/Layout";
+import Navbar from "./components/Navbar/Navbar";
+// import Layout from "./components/Layout/Layout";
 // import MainRoutes from "./components/Routes/MainRoutes";
 function App() {
   const alert = useAlert();
 
   return (
     <BrowserRouter>
-      <Layout>{/* <MainRoutes /> */}</Layout>
+      {/* <Layout>
+        <MainRoutes />
+      </Layout> */}
+      <Navbar />
     </BrowserRouter>
   );
 }
