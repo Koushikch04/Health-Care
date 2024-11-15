efaa6021572945c0a4354e0c987a469fdfc71711
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2024-11-15 18:34:02 +0530

    backend deploy test-6

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,3 @@
 // const baseURL = "http://localhost:8000";
-const baseURL =
-  "https://health-care-backend-nu.vercel.app/?vercelToolbarCode=CutIkbJz8t8UGyt";
-const token = "";
-export { baseURL, token };
+const baseURL = "https://health-care-backend-f9eb.onrender.com";
+export { baseURL };
