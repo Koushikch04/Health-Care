7216d77743484f116173dd5834f41b9e656cd517
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2024-11-22 18:58:45 +0530

    url testing 2

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 // const baseURL = "http://localhost:8000";
 
-console.log(import.meta.env);
+// console.log(import.meta.env);
 
 const baseURL = import.meta.env.VITE_BACKEND || "http://localhost:8000";
 export { baseURL };
