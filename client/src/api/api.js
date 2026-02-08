5d3ed44701d0ed2c6b5603c3096011b0ea675d5e
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2026-02-08 11:36:55 +0530

    update api path

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,2 @@
-// const baseURL = "http://localhost:8000";
-
-// console.log(import.meta.env);
-
 const baseURL = import.meta.env.VITE_BACKEND || "http://localhost:8000";
-console.log(baseURL);
-
 export { baseURL };
