08bf26149ea55189e6cdfb52286730729d0fed11
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2026-02-10 21:56:58 +0530

    fix: simplify uploadedImage initialization in Profile component

--- client/src/components/Navbar/Profile.jsx
+++ client/src/components/Navbar/Profile.jsx
@@ -18,9 +18,8 @@ function Profile() {
   const logoutInProgressRef = useRef(false);
 
   const profileLink = profileImage ? `${baseURL}/${profileImage}` : null;
-  const [uploadedImage, setUploadedImage] = useState(
-    profileLink || "https://bootdey.com/img/Content/avatar/avatar1.png",
-  );
+  const uploadedImage =
+    profileLink || "https://bootdey.com/img/Content/avatar/avatar1.png";
 
   const menuRef = useRef(null);
   const dispatch = useDispatch();
